b8572078bf15bf5cbc0ba4615c35b5f97c30ad6d
pedrokehl <pedrokehl@hotmail.com>
2016-11-23 00:02:23 -0200

    minor changes in server.js file

--- server/server.js
+++ server/server.js
@@ -15,16 +15,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/../frontend'));
 app.use('/api/', app.routes);
 
-consign({cwd: 'server'}).include('controllers').then('routes').into(app);
+consign({cwd: 'server'})
+    .include('controllers')
+    .then('routes')
+    .into(app);
 
-// catch 404
-app.use(function (req, res) {
-    res.status(404).json({error: 'Page Not Found'}).end();
+app.use(function (req, res, next) {
+    next({status: 404});
 });
 
 app.use(errorHandler);
 
-var port = process.env.PORT || '3000';
+var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
     console.log('Express server listening on port %d in %s mode.', port, app.get('env'));
