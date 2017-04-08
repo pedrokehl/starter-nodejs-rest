ddd7e86740934d5788bd152d5b3f9b11cf2d21cd
tigaly <tigaly@qq.com>
2017-04-09 01:57:07 +0800

    Update index.js
    fix code style

--- config/index.js
+++ config/index.js
@@ -2,10 +2,10 @@ const production = require('./production');
 const development = require('./development');
 
 function getConfig() {
-    if (process.env.NODE_ENV === 'production') {
-        return production;
-    }
-    return development;
+  if (process.env.NODE_ENV === 'production') {
+      return production;
+  }
+  return development;
 }
 
 module.exports = getConfig();
